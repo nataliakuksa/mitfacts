dea0ef7e570923a460b5df8a8ef143ed24138e84
Natalia Kuksa <greenkraken@gmail.com>
2021-04-05 00:53:57 -0700

    fixed date, mascot, colors

--- mit.js
+++ mit.js
@@ -1,7 +1,7 @@
 var mit = {
     city: 'Cambridge',
-    colors: ['pink', 'red'],
-    mascot: "robot",
-    founded: 1900,
-    motto: 'Carpe diem',
+    colors: ['Silver Gray', 'red'],
+    mascot: "beaver",
+    founded: 1861,
+    motto: 'Mens at Manus',
 };
